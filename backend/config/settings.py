6618912000f1a5c2ef70e65b4c03ec62d922ecb7
Raghav Sharma <raghav_tinker@yahoo.com>
2021-07-14 00:34:24 +0530

    notes app setup

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party
+    'rest_framework',
+
+    #Local
+    'notes'
+
 ]
 
 MIDDLEWARE = [
