a61a3e37c9957f3958b8aaf25e6f83aa1d643abb
Raghav Sharma <raghav_tinker@yahoo.com>
2021-07-14 02:49:24 +0530

    notes app added models, serializers and views

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -130,3 +130,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
--- backend/notes/views.py
+++ backend/notes/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Note
+from .serializers import NoteSerializer
 
-# Create your views here.
+
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsAuthor
+
+class NoteList(generics.ListAPIView):
+    permission_classes = (IsAuthor, IsAuthenticated)
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
+class NoteCreate(generics.CreateAPIView):
+    permission_classes = (IsAuthor, IsAuthenticated)
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
+class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthor, IsAuthenticated)
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
