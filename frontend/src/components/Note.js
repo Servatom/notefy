6fb354f96686f4e2c6916408f0d9fa08adb2e881
Yashvardhan Arora <yash22arora@gmail.com>
2021-07-22 21:19:21 +0530

    Add Delete, Search feature for notes

--- frontend/src/components/Note.js
+++ frontend/src/components/Note.js
@@ -1,13 +1,42 @@
 import "../assets/css/Notes.css";
-import {MdDeleteForever} from 'react-icons/md';
+import { MdDeleteSweep} from 'react-icons/md';
+import swal from 'sweetalert';
+import { useState } from "react";
+
 
 const Note =(props) =>{
+    
+    
+    const deleteHandler=()=>{
+        swal({
+            title: "Are you sure?",
+            text: "Once deleted, you cannot recover this note!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((willDelete) => {
+            if (willDelete) 
+            {
+              swal("Poof! Your note has been deleted!", {
+                icon: "success",
+              });
+            props.onDelete(props.note.id); 
+            } 
+            else 
+            {
+              swal("Your note is safe!");
+            }
+          });
+    }
+    
+    
     return(
         <div className="note">
             <p>{props.note.text}</p>
             <div className="note-footer">
                 <small>{props.note.date}</small>
-                <MdDeleteForever className="delete-icon"/>
+                <MdDeleteSweep className="delete-icon" onClick={deleteHandler}/>
             </div>
         </div>
     );
