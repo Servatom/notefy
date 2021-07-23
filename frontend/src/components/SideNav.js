ec7dd9f4595130dda7ab8573c452be43c985e4fd
Yashvardhan Arora <yash22arora@gmail.com>
2021-07-24 03:59:38 +0530

    Expand notes, Delete all features

--- frontend/src/components/SideNav.js
+++ frontend/src/components/SideNav.js
@@ -3,12 +3,39 @@ import MoonToggle from "./MoonToggle";
 import logo from "../assets/media/logo.png";
 import {MdDashboard, MdSettings} from 'react-icons/md';
 import {IoLogOut} from 'react-icons/io5';
+import {AiFillDelete} from 'react-icons/ai';
 import { useState } from "react";
+import swal from 'sweetalert';
 
-const SideNav =()=>
+const SideNav =(props)=>
 {   
 
     
+    const allDeleteHandler=()=>
+    {
+        swal({
+            title: "Delete all notes?",
+            text: "This action cannot be reverted!!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((willDelete) => {
+            if (willDelete) 
+            {
+              swal("Poof! Your notes have been deleted!", {
+                icon: "success",
+              });
+            props.deleteAll(); 
+            } 
+            else 
+            {
+              swal("Your notes are safe!");
+            }
+          });
+    }
+
+
     return(
         <div className="sidenav">
             <div className="navhead">
@@ -20,7 +47,11 @@ const SideNav =()=>
             <a><h4 className="active" ><MdDashboard className="icon"/>Dashboard</h4></a>
             <a><h4 className="" ><MdSettings className="icon"/>Settings</h4></a>
             <a><h4 className="" ><IoLogOut className="icon"/>Logout</h4></a>
-            <MoonToggle className="moonNav"/>
+            <div className="sidenavIcons">
+                <MoonToggle className="sidenav-moon"/>
+                <AiFillDelete className="delete-all" onClick={allDeleteHandler}/>
+            </div>
+            
         </div>
         
     );
