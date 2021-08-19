d3e7b85157537fb874545035a82d0d82f45c1a97
rdotjain <rupanshijain45678@gmail.com>
2021-08-19 13:37:54 +0530

    removed email confirmation -.-

--- backend/users/managers.py
+++ backend/users/managers.py
@@ -7,28 +7,22 @@ class UserManager(BaseUserManager):
     Custom User Manager to use email as unique identifier
     """
 
-    def create_user(self, email, password, **extra_fields):
-        """
-        Create and save a user with given email and password
-        """
+    def create_user(self, email, name, password=None):
         if not email:
-            raise ValueError(_("User must have an email address"))
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+            raise ValueError("Email address required")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+        )
+        user.is_active = True
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
-        """
-        Create and save a SuperUser with given email and password
-        """
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError(_("Superuser must have is_staff=True."))
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
-        return self.create_user(email, password, **extra_fields)
+    def create_superuser(self, email, name, password=None):
+        user = self.create_user(email, name, password=password)
+        user.is_admin = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
--- backend/users/models.py
+++ backend/users/models.py
@@ -13,7 +13,6 @@ class User(AbstractBaseUser, PermissionsMixin):
         max_length=255, unique=True, verbose_name="email address"
     )
     name = models.CharField("Name", max_length=20)
-    is_email_confirmed = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
