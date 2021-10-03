233d4c47425baa41f775861d6e0d5a3e38ac61d9
Om Kathe <omkathe26@gmail.com>
2021-10-03 17:55:28 +0530

    feat: added-responsive-navbar

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,206 +1,231 @@
-import Searchbar from '../components/Searchbar';
-import NotesList from '../components/NotesList';
-import '../assets/css/Dashboard.css';
-import { useState, useEffect, useContext } from 'react';
-import { nanoid } from 'nanoid';
-import SideNav from '../components/SideNav';
-import logo from '../assets/media/logo.png';
-import URL from '../URL';
-import AuthContext from '../store/auth-context';
-import Settings from '../components/Settings';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Searchbar from "../components/Searchbar";
+import NotesList from "../components/NotesList";
+import "../assets/css/Dashboard.css";
+import { useState, useEffect, useContext } from "react";
+import { ImCross, ImMenu } from "react-icons/im";
+import { HiMenu } from "react-icons/hi";
+import { nanoid } from "nanoid";
+import SideNav from "../components/SideNav";
+import logo from "../assets/media/logo.png";
+import URL from "../URL";
+import AuthContext from "../store/auth-context";
+import Settings from "../components/Settings";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const Dashboard = (props) => {
-  const [notes, setNotes] = useState([
-    // {
-    //     id:nanoid(),
-    //     text:"This is dummy note 1",
-    //     date:"18/7/2021"
-    // },
-  ]);
+    const [notes, setNotes] = useState([
+        // {
+        //     id:nanoid(),
+        //     text:"This is dummy note 1",
+        //     date:"18/7/2021"
+        // },
+    ]);
+    const [isOpen, setIsOpen] = useState(false);
+    const authCtx = useContext(AuthContext);
 
-  const authCtx = useContext(AuthContext);
+    useEffect(() => {
+        // let savedNotes = JSON.parse(localStorage.getItem('notes-local'))
 
-  useEffect(() => {
-    // let savedNotes = JSON.parse(localStorage.getItem('notes-local'))
+        // if(savedNotes)
+        // setNotes(savedNotes);
 
-    // if(savedNotes)
-    // setNotes(savedNotes);
-
-    getNotes();
-    getUserDetails();
-  }, []);
-
-  // stores notes to local storage whenever the notes list changes
-  // useEffect(() => {
-  //     localStorage.setItem('notes-local',JSON.stringify(notes));
-  // }, [notes]);
-
-  const [userInfo, setUserInfo] = useState({});
-  const getUserDetails = () => {
-    var myHeaders = new Headers();
-    myHeaders.append('Authorization', 'Token ' + authCtx.token);
-
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow',
-    };
-
-    fetch('https://notefyapi.servatom.com/api/users/detail/', requestOptions)
-      .then((response) => response.json())
-      .then((result) => setUserInfo(result))
-      .catch((error) => console.log('error', error));
-  };
-
-  const [searchText, setSearchText] = useState('');
-
-  //new note handler
-  const newNoteHandler = (newnote) => {
-    // setNotes([newnote, ...notes]);
-    var myHeaders = new Headers();
-    myHeaders.append('Authorization', 'Token ' + authCtx.token);
-    myHeaders.append('Content-Type', 'application/json');
-
-    var raw = JSON.stringify({
-      title: 'k',
-      body: newnote.text,
-    });
-
-    var requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow',
-    };
-
-    fetch('https://notefyapi.servatom.com/api/notes/create/', requestOptions)
-      .then((response) => response.text())
-      .then((result) => {
         getNotes();
-      })
-      .catch((error) => console.log('error', error));
-  };
+        getUserDetails();
+    }, []);
 
-  //fetching notes from database
-  const getNotes = () => {
-    var myHeaders = new Headers();
-    myHeaders.append('Authorization', 'Token ' + authCtx.token);
+    // stores notes to local storage whenever the notes list changes
+    // useEffect(() => {
+    //     localStorage.setItem('notes-local',JSON.stringify(notes));
+    // }, [notes]);
 
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow',
+    const [userInfo, setUserInfo] = useState({});
+    const getUserDetails = () => {
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", "Token " + authCtx.token);
+
+        var requestOptions = {
+            method: "GET",
+            headers: myHeaders,
+            redirect: "follow",
+        };
+
+        fetch(
+            "https://notefyapi.servatom.com/api/users/detail/",
+            requestOptions
+        )
+            .then((response) => response.json())
+            .then((result) => setUserInfo(result))
+            .catch((error) => console.log("error", error));
     };
 
-    fetch('https://notefyapi.servatom.com/api/notes/', requestOptions)
-      .then((response) => response.json())
-      .then((result) => setNotes(result))
-      .catch((error) => console.log('error', error));
-  };
+    const [searchText, setSearchText] = useState("");
 
-  //deleting notes
-  const deleteNoteHandler = (id) => {
-    // const newNotes = notes.filter((note)=>note.id!==id);
-    // setNotes(newNotes);
+    //new note handler
+    const newNoteHandler = (newnote) => {
+        // setNotes([newnote, ...notes]);
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", "Token " + authCtx.token);
+        myHeaders.append("Content-Type", "application/json");
 
-    var myHeaders = new Headers();
-    myHeaders.append('Authorization', 'Token ' + authCtx.token);
+        var raw = JSON.stringify({
+            title: "k",
+            body: newnote.text,
+        });
 
-    var requestOptions = {
-      method: 'DELETE',
-      headers: myHeaders,
-      redirect: 'follow',
+        var requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow",
+        };
+
+        fetch(
+            "https://notefyapi.servatom.com/api/notes/create/",
+            requestOptions
+        )
+            .then((response) => response.text())
+            .then((result) => {
+                getNotes();
+            })
+            .catch((error) => console.log("error", error));
     };
 
-    fetch(`https://notefyapi.servatom.com/api/notes/${id}/`, requestOptions)
-      .then((response) => {
-        // console.log(response.status);
+    //fetching notes from database
+    const getNotes = () => {
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", "Token " + authCtx.token);
+
+        var requestOptions = {
+            method: "GET",
+            headers: myHeaders,
+            redirect: "follow",
+        };
+
+        fetch("https://notefyapi.servatom.com/api/notes/", requestOptions)
+            .then((response) => response.json())
+            .then((result) => setNotes(result))
+            .catch((error) => console.log("error", error));
+    };
+
+    //deleting notes
+    const deleteNoteHandler = (id) => {
+        // const newNotes = notes.filter((note)=>note.id!==id);
+        // setNotes(newNotes);
+
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", "Token " + authCtx.token);
+
+        var requestOptions = {
+            method: "DELETE",
+            headers: myHeaders,
+            redirect: "follow",
+        };
+
+        fetch(`https://notefyapi.servatom.com/api/notes/${id}/`, requestOptions)
+            .then((response) => {
+                // console.log(response.status);
+                getNotes();
+            })
+            .catch((error) => console.log("error", error));
+    };
+
+    //editing note
+    const editNoteHandler = (editedNote) => {
+        // const newNotes = notes.filter((note)=>note.id!==editedNote.id);
+        // setNotes([editedNote, ...newNotes]);
+
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", "Token " + authCtx.token);
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
+            title: "k",
+            body: editedNote.text,
+        });
+
+        var requestOptions = {
+            method: "PUT",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow",
+        };
+
+        fetch(
+            `https://notefyapi.servatom.com/api/notes/${editedNote.id}/`,
+            requestOptions
+        )
+            .then((response) => {
+                getNotes();
+                return response.json();
+            })
+            .then((result) => console.log(result))
+            .catch((error) => console.log("error", error));
+    };
+
+    const deleteAllNotes = () => {
+        notes.map((note) => {
+            deleteNoteHandler(note.id);
+        });
         getNotes();
-      })
-      .catch((error) => console.log('error', error));
-  };
-
-  //editing note
-  const editNoteHandler = (editedNote) => {
-    // const newNotes = notes.filter((note)=>note.id!==editedNote.id);
-    // setNotes([editedNote, ...newNotes]);
-
-    var myHeaders = new Headers();
-    myHeaders.append('Authorization', 'Token ' + authCtx.token);
-    myHeaders.append('Content-Type', 'application/json');
-
-    var raw = JSON.stringify({
-      title: 'k',
-      body: editedNote.text,
-    });
-
-    var requestOptions = {
-      method: 'PUT',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow',
     };
 
-    fetch(
-      `https://notefyapi.servatom.com/api/notes/${editedNote.id}/`,
-      requestOptions
-    )
-      .then((response) => {
-        getNotes();
-        return response.json();
-      })
-      .then((result) => console.log(result))
-      .catch((error) => console.log('error', error));
-  };
+    const watermark = () => {
+        return (
+            <div className="watermark">
+                <img src={logo} />
+                <p>It feels so empty here ;( ... Add some notes!</p>
+            </div>
+        );
+    };
 
-  const deleteAllNotes = () => {
-    notes.map((note) => {
-      deleteNoteHandler(note.id);
-    });
-    getNotes();
-  };
+    const [currentTab, setCurrentTab] = useState(1);
 
-  const watermark = () => {
     return (
-      <div className='watermark'>
-        <img src={logo} />
-        <p>It feels so empty here ;( ... Add some notes!</p>
-      </div>
-    );
-  };
-
-  const [currentTab, setCurrentTab] = useState(1);
-
-  return (
-    <div className='dashboard'>
-      <div className='side-nav'>
-        <SideNav
-          deleteAll={deleteAllNotes}
-          setCurrentTab={setCurrentTab}
-          currentTab={currentTab}
-        /> 
-      </div>
-      <div className='cont'>
-        <Switch>
-          <Route exact path='/dashboard'>
-            <Searchbar searchHandler={setSearchText} />
-            <NotesList
-              notes={notes.filter((note) =>
-                note.body.toLowerCase().includes(searchText)
-              )}
-              addNew={newNoteHandler}
-              deleteNote={deleteNoteHandler}
-              editNote={editNoteHandler}
-            />
-            {!notes.length ? watermark() : null}
-          </Route>
-          <Route path={'/dashboard/settings'}>
-            <Settings userInfo={userInfo} setUserInfo={setUserInfo} />
-          </Route>
-        </Switch>
-      </div>
-      {/* <div className='cont'>
+        <>
+            <div
+                onClick={() => {
+                    setIsOpen(!isOpen);
+                }}
+                className="nav-menu"
+            >
+                {isOpen ? (
+                    <ImCross className="nav-menu-icon" />
+                ) : (
+                    <HiMenu className="nav-menu-icon" />
+                )}
+            </div>
+            <div className="dashboard">
+                <div className={isOpen ? 'side-nav-open' : 'side-nav'}>
+                    <SideNav
+                        deleteAll={deleteAllNotes}
+                        setCurrentTab={setCurrentTab}
+                        currentTab={currentTab}
+                        closeNav = {()=>{setIsOpen(false)}}
+                    />
+                </div>
+                <div className="cont">
+                    <Switch>
+                        <Route exact path="/dashboard">
+                            <Searchbar searchHandler={setSearchText} />
+                            <NotesList
+                                notes={notes.filter((note) =>
+                                    note.body.toLowerCase().includes(searchText)
+                                )}
+                                addNew={newNoteHandler}
+                                deleteNote={deleteNoteHandler}
+                                editNote={editNoteHandler}
+                            />
+                            {!notes.length ? watermark() : null}
+                        </Route>
+                        <Route path={"/dashboard/settings"}>
+                            <Settings
+                                userInfo={userInfo}
+                                setUserInfo={setUserInfo}
+                            />
+                        </Route>
+                    </Switch>
+                </div>
+                {/* <div className='cont'>
         {currentTab === 1 ? (
           <>
             <Searchbar searchHandler={setSearchText} />
@@ -218,8 +243,9 @@ const Dashboard = (props) => {
           <Settings userInfo={userInfo} setUserInfo={setUserInfo} />
         )}
       </div> */}
-    </div>
-  );
+            </div>
+        </>
+    );
 };
 
 export default Dashboard;
