2ee3206bbe595b5499c0b7e3c944b07e7e1277a0
Raghav Sharma <raghav_tinker@yahoo.com>
2021-10-04 13:35:37 +0530

    Todo app reconfigured with categories

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 from django.conf import settings
 
-class Todo(models.Model):
+class ToDoCategory(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    to_do = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.category
+
+class ToDoItem(models.Model):
+    category = models.ForeignKey(ToDoCategory, on_delete=models.CASCADE)
+    item = models.CharField(max_length=100)
     isDone = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return self.item
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,22 +1,111 @@
 from rest_framework import generics
-from .models import Todo
-from .serializers import TodoSerializer
-
+from rest_framework.views import APIView
+from .models import ToDoCategory, ToDoItem
+from .serializers import ToDoItemSerializer, ToDoCategorySerializer
+from rest_framework.response import Response
 
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsAuthor
 
-class ToDoList(generics.ListAPIView):
+class ToDoCategoryView(APIView):
     permission_classes = (IsAuthor, IsAuthenticated)
-    serializer_class = TodoSerializer
-    def get_queryset(self):
-        return Todo.objects.filter(author__id=self.request.user.id)
+    serializer_class = ToDoCategorySerializer
+    
+    def get(self, request):
+        # get all categories of author
+        categories = ToDoCategory.objects.filter(author=request.user)
+        serializer = ToDoCategorySerializer(categories, many=True)
+        return Response(serializer.data)
+        
+    def post(self, request):
+        # create new category
+        category_name = request.data['category']
+        category = ToDoCategory(category=category_name, author=request.user)
+        category.save()
+        serializer = ToDoCategorySerializer(category)
+        return Response(serializer.data)
+    
+    def put(self, request):
+        # update category
+        category = ToDoCategory.objects.get(id=request.data['cat_id'])
+        category.category = request.data['category']
+        category.save()
+        serializer = ToDoCategorySerializer(category)
+        return Response(serializer.data)
+    
+    def delete(self, request):
+        # delete category
+        try:
+            category = ToDoCategory.objects.get(author=request.user, id=request.data['cat_id'])
+            category.delete()
 
-class ToDoCreate(generics.CreateAPIView):
-    permission_classes = (IsAuthor, IsAuthenticated)
-    serializer_class = TodoSerializer
+            # delete all items of category
+            items = ToDoItem.objects.filter(category=category)
+            items.delete()
+            return Response({"message": "Successfully Deleted"},status=204)
+        except:
+            return Response({"message": "Error"})
 
-class ToDoDetail(generics.RetrieveUpdateDestroyAPIView):
+
+# list all to do items in todo category
+class ToDoItemView(APIView):
     permission_classes = (IsAuthor, IsAuthenticated)
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
+    serializer_class = ToDoItemSerializer
+    
+    def get(self, request):
+        if 'cat_id' not in request.data:
+            todo_items = ToDoItem.objects.filter(category__author=request.user)
+            serializer = ToDoItemSerializer(todo_items, many=True)
+            return Response(serializer.data)
+
+        # check if category id exists
+        if not ToDoCategory.objects.filter(id=request.data['cat_id']).exists():
+            return Response({"message": "Category not found"},status=404)
+        
+        category_id = request.data['cat_id']
+        todo_items = ToDoItem.objects.filter(category__author=request.user,category__id=category_id)
+        serializer = ToDoItemSerializer(todo_items, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        category_id = request.data['cat_id']
+        if not ToDoCategory.objects.filter(author=request.user ,id=category_id).exists():
+            return Response({"message": "Category not found"},status=404)
+        
+        # check if is_done given if not then default false
+        isDone = False
+        if 'is_done' in request.data:
+            isDone = request.data['is_done']
+        todo_item = ToDoItem.objects.create(category_id=category_id, item=request.data['item'], isDone=isDone)
+        todo_item.save()
+
+        serializer = ToDoItemSerializer(todo_item)
+        return Response(serializer.data)
+    
+    def put(self, request):
+        todo_item = request.data['todo_item_id']
+        
+        if not ToDoItem.objects.filter(category__author=request.user, id=todo_item).exists():
+            return Response({"message": "Todo item not found"},status=404)
+
+        todo_item = ToDoItem.objects.get(category__author=request.user, id=todo_item)
+        if 'item' in request.data:
+            todo_item.item = request.data['item']
+        if 'is_done' in request.data:
+            todo_item.isDone = request.data['is_done']
+        
+        todo_item.save()
+        serializer = ToDoItemSerializer(todo_item)
+        return Response(serializer.data)
+    
+    def delete(self, request):
+        if not ToDoItem.objects.filter(id=request.data['todo_item_id']).exists():
+            return Response({"message": "Todo item not found"},status=404)
+
+        todo_item = request.data['todo_item_id']
+        try:
+            todo_item = ToDoItem.objects.get(category__author=request.user,id=todo_item)
+            todo_item.delete()
+            return Response({"message": "successfully deleted"},status=204)
+        except:
+            return Response({"message": "Error"},status=400)
