83f55f90fcafca0bd6cddc2559c025b30797d572
Nirbhay Makhija <nmakhija_be20@thapar.edu>
2021-10-05 22:15:09 +0530

    api call for list

--- App/lib/main.dart
+++ App/lib/main.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
+import 'package:app/models/notes.dart';
 import 'package:app/routers/approutes.dart';
 import 'package:app/routers/routenames.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider.value(value: Auth()),
+        ChangeNotifierProvider.value(value: Notes()),
       ],
       child: MaterialApp(
           onGenerateRoute: AppRoutes.onGenerateRoute,
--- App/lib/models/notes.dart
+++ App/lib/models/notes.dart
@@ -1,62 +1,49 @@
 // ignore_for_file: prefer_final_fields
 
 import 'package:flutter/cupertino.dart';
-
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 import 'dart:convert';
-
 import 'auth.dart';
 import 'note.dart';
 
 class Notes with ChangeNotifier {
   // String key = Provider.of<Auth>(_,listen: false).getKey();
-  List<Note> _notes = [
-    Note(
-      body:
-          'Hello thereHello thereHello thereHello thereHello thereHello thereHello thereHello thereHello there',
-      id: '1',
-      title: 'Note1',
-    ),
-    Note(
-      body: 'Hello world',
-      id: '2',
-      title: 'Note2',
-    ),
-    Note(
-      body: 'Hello',
-      id: '3',
-      title: 'Note3',
-    ),
-    Note(
-      body: 'Hello matar',
-      id: '4',
-      title: 'Note4',
-    ),
-  ];
+  String key = '59197db1928e55a3b92d3528e023dc78c0748b91';
+  List<Note> _notes = [];
 
   List<Note> get notesList {
     return [..._notes];
   }
 
-  void addNote(String title, String body) {
-    _notes.add(Note(body: body, id: '10', title: title));
-  }
-
   // API calls
-  // Future getList() async {
-  //   try {
-  //     http.Response response = await http.get(
-  //       Uri.parse('https://notefyapi.servatom.com/api/users/detail/'),
-  //       headers: {'Authorization': 'Token $key'},
-  //     );
+  Future getList() async {
+    try {
+      http.Response response = await http.get(
+        Uri.parse('https://notefyapi.servatom.com/api/notes/'),
+        headers: {'Authorization': 'Token $key'},
+      );
 
-  //     String d = response.body;
-  //     var data = jsonDecode(d);
+      String d = response.body;
+      var data = jsonDecode(d);
 
-  //     print(response.body);
-  //     if (response.statusCode == 200) {
-  //     } else {}
-  //   } catch (e) {}
-  // }
+      print(response.body);
+      if (response.statusCode == 200) {
+        List tempNote = data;
+        for (int i = 0; i < tempNote.length; i++) {
+          _notes[i] = Note(
+            body: tempNote[i]["body"],
+            title: tempNote[i]["title"],
+            id: tempNote[i]["id"],
+            createTime: tempNote[i]["created_at"],
+            updateTime: tempNote[i]["updated_at"],
+          );
+        }
+      } else {
+        throw 'Error';
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
 }
