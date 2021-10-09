33bdd93ca65fb8645f2e830ceda860a062f5a68d
rdotjain <rupanshijain45678@gmail.com>
2021-10-09 22:01:34 +0530

    Use environ to manage secrets

--- backend/users/verify_email.py
+++ backend/users/verify_email.py
@@ -1,15 +1,15 @@
-import os
 from django.core.mail import send_mail, EmailMultiAlternatives
 from django.utils.crypto import get_random_string
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.contrib.auth import get_user_model
+from django.conf import settings
 
+FRONTEND_URL = settings.FRONTEND_URL
 
 def send_verify_email(to_mail, from_mail, verify_link):
-    FRONTEND_URL = os.getenv('FRONTEND_URL')
     token = get_random_string(length=32)
-    verify_link = FRONTEND_URL + 'email-verify/' + token
+    verify_link = FRONTEND_URL + '/email-verify/' + token
     html_content = render_to_string(
         'users/verify_mail_template.html', {'FRONTEND_URL': FRONTEND_URL, 'link': verify_link})  # render with dynamic value
     # Strip the html tag. So people can see the pure text at least.
@@ -27,9 +27,8 @@ def send_verify_email(to_mail, from_mail, verify_link):
 
 
 def send_reset_email(to_mail, from_mail):
-    FRONTEND_URL = os.getenv('FRONTEND_URL')
     token = get_random_string(length=32)
-    reset_link = FRONTEND_URL + 'reset-password/' + token
+    reset_link = FRONTEND_URL + '/reset-password/' + token
     html_content = render_to_string(
         'users/reset_mail_template.html', {'FRONTEND_URL': FRONTEND_URL, 'link': reset_link})
 
