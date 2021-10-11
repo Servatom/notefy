6fdb07f72acde7a027cf139053632d1b86e502c3
Shreyasrana18 <srana1_be20@thapar.edu>
2021-10-12 00:10:24 +0530

    added hide and show appbar on scroll

--- App/lib/screens/dashboard.dart
+++ App/lib/screens/dashboard.dart
@@ -1,4 +1,6 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, avoid_print, unused_import
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, avoid_print, unused_import, curly_braces_in_flow_control_structures
+
+
 
 import 'package:app/models/auth.dart';
 import 'package:app/components/drawer.dart';
@@ -8,74 +10,105 @@ import 'package:app/models/note.dart';
 import 'package:app/models/notes.dart';
 import 'package:app/routers/routenames.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 
-class DashBoard extends StatelessWidget {
+class DashBoard extends StatefulWidget {
+  @override
+  State<DashBoard> createState() => _DashBoardState();
+}
+
+class _DashBoardState extends State<DashBoard> {
+  bool isVisible = true;
+
   @override
   Widget build(BuildContext context) {
     List notesList = Provider.of<Notes>(context, listen: true).notesList;
     return Scaffold(
-        // backgroundColor: kbgcolor,
-        drawer: DashboardDrawer(),
-        appBar: AppBar(
-          // backgroundColor: kyellow,
-          iconTheme: IconThemeData(color: kbgcolor),
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Notefy',
-                style: TextStyle(
-                  color: kbgcolor,
-                  fontFamily: 'roboto',
+      drawer: DashboardDrawer(),
+      body: NestedScrollView(
+        floatHeaderSlivers: true,
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          SliverAppBar(
+            floating: true,
+            snap: true,
+            iconTheme: IconThemeData(color: kbgcolor),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Notefy',
+                  style: TextStyle(
+                    color: kbgcolor,
+                    fontFamily: 'roboto',
+                  ),
                 ),
-              ),
-              Image.asset(
-                'images/logo.png',
-                scale: 10,
-              ),
+                Image.asset(
+                  'images/logo.png',
+                  scale: 10,
+                ),
+              ],
+            ),
+            centerTitle: true,
+            actions: [
+              IconButton(
+                onPressed: () {
+                  print('hello');
+                },
+                icon: Icon(
+                  Icons.search,
+                ),
+              )
             ],
           ),
-          centerTitle: true,
-          actions: [
-            IconButton(
-              onPressed: () {
-                print('hello');
-              },
-              icon: Icon(
-                Icons.search,
-              ),
-            )
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          // backgroundColor: Theme.of(context).primaryColor,
-          onPressed: () {
-            Navigator.pushNamed(
-              context,
-              RouteNames.noterscreen,
-              arguments: Note(
-                body: '',
-                title: '',
-                id: '',
-                createTime: '',
-                updateTime: '',
-              ),
-            );
+        ],
+        body: NotificationListener<UserScrollNotification>(
+          onNotification: (notification) {
+            if (notification.direction == ScrollDirection.forward) {
+              if (!isVisible)
+                setState(() {
+                  isVisible = true;
+                });
+            } else if (notification.direction == ScrollDirection.reverse) {
+              if (isVisible)
+                setState(() {
+                  isVisible = false;
+                });
+            }
+            return true;
           },
-          child: Icon(
-            Icons.add,
-            color: kbgcolor,
-            size: 30,
+          child: ListView.builder(
+            itemCount: notesList.length,
+            itemBuilder: (context, index) {
+              return NoteTile(
+                note: notesList[index],
+              );
+            },
           ),
         ),
-        body: ListView.builder(
-          itemCount: notesList.length,
-          itemBuilder: (context, index) {
-            return NoteTile(
-              note: notesList[index],
-            );
-          },
-        ));
+      ),
+      floatingActionButton: isVisible
+          ? FloatingActionButton(
+              onPressed: () {
+                Navigator.pushNamed(
+                  context,
+                  RouteNames.noterscreen,
+                  arguments: Note(
+                    body: '',
+                    title: '',
+                    id: '',
+                    createTime: '',
+                    updateTime: '',
+                  ),
+                );
+              },
+              child: Icon(
+                Icons.add,
+                color: kbgcolor,
+                size: 30,
+              ),
+            )
+          : null,
+    );
   }
 }
