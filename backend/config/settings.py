63f540811ff1bdfab41d4619c2984b0f7b0d7930
Raghav Sharma <raghav_tinker@yahoo.com>
2021-10-18 00:28:05 +0530

    Uncommenting databases variable

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -78,14 +78,13 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
-"""
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-"""
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
